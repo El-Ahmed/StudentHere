33c34cca3302a286046ccd1e6b461db2ad742dbe
Ahmed El herz <elherzahmed@gmail.com>
2022-12-24 22:42:52 +0100

    new activity and start it with the qr button

--- app/src/main/java/ma/ac/emi/studenthere/MainActivity.java
+++ app/src/main/java/ma/ac/emi/studenthere/MainActivity.java
@@ -2,7 +2,10 @@ package ma.ac.emi.studenthere;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, QRScanner.class);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> startActivity(intent));
     }
 }
