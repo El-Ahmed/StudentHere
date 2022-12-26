604843c36ebe7e13ba0fcd8be7046167bbcd910c
Ahmed El herz <elherzahmed@gmail.com>
2022-12-26 23:09:39 +0100

    getting course online

--- app/src/main/java/ma/ac/emi/studenthere/MainActivity.java
+++ app/src/main/java/ma/ac/emi/studenthere/MainActivity.java
@@ -5,22 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int QRCODE_RESULT = 101;
+    private TextView attendanceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, QRScanner.class);
         Button button = findViewById(R.id.button);
+        attendanceView = findViewById(R.id.attendance);
+
+        Intent intent = new Intent(this, QRScanner.class);
         button.setOnClickListener(view ->
                 startActivityForResult(intent, QRCODE_RESULT ));
+
     }
 
     @Override
@@ -30,11 +41,42 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             if (data != null) {
                 String qrCode = data.getData().toString();
-                Toast.makeText(this,qrCode,Toast.LENGTH_SHORT).show();
+
+                getCourse();
             }
-
-
+            else {
+                Toast.makeText(this,"failed",Toast.LENGTH_SHORT).show();
+            }
         }
+        else {
+            Toast.makeText(this,"failed",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void getCourse() {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://c3b6072f-1301-40fe-b039-f0ed1358da3b.mock.pstmn.io/course";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        attendanceView.setText("Attending " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                attendanceView.setText("Failed. Try again");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
 }
